91fb79ed0eea89011a66e068c5d06d45a1d3af32
Marti Vilella <marti.vilella@esa.int>
2021-04-09 16:09:48 +0200

    Refactor to more intuitive structure with new gp_engine methods

--- src/gp_node.cpp
+++ src/gp_node.cpp
@@ -8,16 +8,22 @@
 
 using namespace std::chrono_literals;
 
-std::string target_octree_filename = "assets/bt/ENVISAT_midres.bt";
-std::string gripper_octree_filename = "assets/bt/2F85_gripper_midres.bt";
+std::string target_bintree_filename = "assets/bt/ENVISAT_midres.bt";
+std::string target_octree_filename = "assets/ot/ENVISAT_midres.ot";
+std::string gripper_bintree_filename = "assets/bt/2F85_gripper_midres.bt";
 
 int main()
-{
-    //graspQualityMap gqm{0.1};
-    //gqm.build_simple_gripper();
-    
-    octomap::OcTree target_tree(target_octree_filename);
-    octomap::OcTree gripper_tree(gripper_octree_filename);
+{    
+    #define ot // bt -> import from binarytrees, ot-> from object trees (precompiled, faster)
+    #ifdef bt
+    octomap::OcTree target_tree(target_bintree_filename);
+    octomap::OcTree gripper_tree(gripper_bintree_filename);
+    #endif
+    #ifdef ot
+    octomap::OcTreeGraspQuality* target_tree = new octomap::OcTreeGraspQuality(target_octree_filename);
+    octomap::OcTree gripper_tree(gripper_bintree_filename); // TODO Compile and switch to .ot model
+    #endif
+
 
     // xyz min -0.04,0.115,0.085
     octomap::point3d min_point3d{-0.04,0.115,0.085};
@@ -25,15 +31,14 @@ int main()
     octomap::point3d max_point3d{0.04,0.145,0.102};
 
     graspQualityMap gqm{0.1};
+    
     gqm.set_target_tree(target_tree);
     gqm.set_gripper_tree(gripper_tree, min_point3d, max_point3d);
     
-    octomap::ColorOcTree grasp_visualisation{gqm.visualise_grasp()};
+    gqm.update_global_grasp_quality(0);
 
-    grasp_visualisation.write("grasp_visualisation.ot");
+    //gqm.get_target_tree()->write("ENVISAT_midres.ot");
     
-    //gqm.update_global_grasp_quality(0);
-    
-    //((octomap::ColorOcTree)*gqm.get_gripper_tree()).write("grippercolor.ot");
+    //((octomap::ColorOcTree)*gqm.get_target_tree()).write("targetcolor.ot");
     return 0;
 }
